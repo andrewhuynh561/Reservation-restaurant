dc264b17017cd68298a570d40b66d66885fb8078
Duy Quoc Anh Nguyen <84012972+AnhNotAnh@users.noreply.github.com>
2023-09-14 12:43:08 +0930

    Change banquet textbox

--- web_app/src/routes/Booking.jsx
+++ web_app/src/routes/Booking.jsx
@@ -224,38 +224,38 @@ function Booking() {
                 Selected Time: {timeslot && timeslot.timeSlot}
               </p>{" "}
               {/* there to see if the time is updated and displayed */}
-              {banquets.length > 0 && (
-                <div className="mt-3 mb-3">
-                  <h3 className="word">Select your banquet option</h3>
-                  <select
-                    onChange={(e) => {
-                      updateBanquet(e.target);
-                    }}
-                    style={{ width: "200px", height: "30px" }}
-                    id="banquetOptions"
-                    name="banquetOptions"
-                    form="banquetForm"
-                  >
-                    <option value={-1}>None</option>
-                    {banquets.map((banquet) => (
-                      <option key={banquet.banquetID} value={banquet.banquetID}>
-                        {banquet.banquetName} {banquet.banquetPrice}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              )}
+            </div>
+            {banquets.length > 0 && (
               <div className="mt-3 mb-3">
-                <h4 className="word">Select number of guests</h4>
-                <input
-                  name="numberOfGuests"
-                  type="number"
-                  min="0"
-                  value={guest}
-                  onChange={(e) => setGuest(e.target.value)}
-                  style={{ width: "100%" }}
-                />
+                <h3 className="word">Select your banquet option</h3>
+                <select
+                  onChange={(e) => {
+                    updateBanquet(e.target);
+                  }}
+                  style={{ width: "100%", height: "30px" }}
+                  id="banquetOptions"
+                  name="banquetOptions"
+                  form="banquetForm"
+                >
+                  <option value={-1}>None</option>
+                  {banquets.map((banquet) => (
+                    <option key={banquet.banquetID} value={banquet.banquetID}>
+                      {banquet.banquetName} {banquet.banquetPrice}
+                    </option>
+                  ))}
+                </select>
               </div>
+            )}
+            <div className="mt-3 mb-3">
+              <h4 className="word">Select number of guests</h4>
+              <input
+                name="numberOfGuests"
+                type="number"
+                min="0"
+                value={guest}
+                onChange={(e) => setGuest(e.target.value)}
+                style={{ width: "100%" }}
+              />
             </div>
           </form>
           {selectedBanquetID != -1 && <Payment></Payment>}
