ea34ad0a1c61b00af980a998ab44cc05bca1c114
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2023-10-02 16:47:16 +1030

    created a modal confirmation

--- web_app/src/routes/Profile.jsx
+++ web_app/src/routes/Profile.jsx
@@ -10,9 +10,17 @@ function Profile() {
   const navigate = useNavigate();
   const accountID=history.state.usr.accountDetails.accountID
   const userName=history.state.usr.accountDetails.username
- 
+  const [isConfirmationModalOpen, setConfirmationModalOpen] = useState(false);
 
- 
+  const openConfirmationModal = () => {
+    setConfirmationModalOpen(true);
+  };
+
+  const closeConfirmationModal = (e) => {
+    e.preventDefault();
+    setConfirmationModalOpen(false);
+  };
+
 
 
   useEffect(() => {
@@ -46,6 +54,68 @@ function Profile() {
         </div>
       )} */}
       </div>
+      <Modal
+          isOpen={isConfirmationModalOpen}
+          onRequestClose={closeConfirmationModal}
+          contentLabel="Confirmation Modal"
+          style={{
+            overlay: {
+              position: "fixed",
+              zIndex: 1020,
+              top: 0,
+              left: 0,
+              width: "100vw",
+              height: "100vh",
+              background: "rgba(255, 255, 255, 0.5)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            },
+            content: {
+              background: "white",
+              width: "45rem",
+              maxWidth: "calc(100vw - 2rem)",
+              maxHeight: "calc(100vh - 2rem)",
+              overflowY: "auto",
+              position: "relative",
+              border: "1px solid #ccc",
+              borderRadius: "0.5rem",
+            },
+          }}
+        >
+          <div class="modal-dialog modal-confirm">
+            <div class="modal-content">
+              <div class="modal-header">
+                <div class="icon-box">
+                  <i class="material-icons">&#xE876;</i>
+                </div>
+                <h2 class="modal-title w-100">
+                  Are you sure to delete this account?
+                </h2>
+              </div>
+              <hr />
+              
+              <div class="modal-footer">
+                <button
+                  class="btn btn-success btn-block"
+                  data-dismiss="modal"
+                  onClick={closeConfirmationModal}
+                >
+                  Cancel
+                </button>
+              </div>
+              <div class="modal-footer">
+                <button
+                  class="btn btn-success btn-block"
+                  data-dismiss="modal"
+                 
+                >
+                  OK
+                </button>
+              </div>
+            </div>
+          </div>
+        </Modal>
       
     </>
   );
