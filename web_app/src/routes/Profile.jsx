1266e095f1bc279fd867e9c4c1cedf61c0281df4
Chanh Hai Huynh <huyhc001@mymail.unisa.edu.au>
2023-10-02 16:45:59 +1030

    get accountID and userName in profile page

--- web_app/src/routes/Profile.jsx
+++ web_app/src/routes/Profile.jsx
@@ -1,21 +1,54 @@
 import React, {useState, useEffect} from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import Modal from "react-modal";
+import "./Booking.css";
+
 
 
 function Profile() {
   document.body.id = 'H';
   const navigate = useNavigate();
+  const accountID=history.state.usr.accountDetails.accountID
+  const userName=history.state.usr.accountDetails.username
+ 
+
+ 
+
 
   useEffect(() => {
     console.log("user", history.state.usr)
+    console.log("accountID:", accountID);
+    console.log("user name:", userName);
     if(history.state.usr === undefined) {
         console.log("got here");
         navigate(`/`);
     }
   }, [history.state.usr]);
 
-  return <h1>Welcome back, Mate 
-    {(history.state.usr.currentTier != undefined) && <div>Youre in tier {history.state.usr.currentTier.tierName}</div>}
-  </h1>
+  return (
+    <>
+      <div>
+      <h1>Welcome back, Mate</h1>
+      {(history.state.usr.currentTier != undefined) && <div>You're in tier {history.state.usr.currentTier.tierName}</div>}
+
+     
+      <button onClick={openConfirmationModal} className="btn btn-danger">Delete Account</button>
+
+      {/* Delete Account Confirmation Modal */}
+      {/* {isDeleteModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>Confirm Account Deletion</h3>
+            <p>Are you sure you want to delete your account? This action cannot be undone.</p>
+            <button onClick={handleDeleteAccount} className="btn btn-danger">Delete</button>
+            <button onClick={closeDeleteModal} className="btn btn-secondary">Cancel</button>
+          </div>
+        </div>
+      )} */}
+      </div>
+      
+    </>
+  );
 }
-export default Profile
+
+export default Profile;
